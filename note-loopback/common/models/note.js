29474e982f60762822ebbc7c0bb34feeeb0aad8b
Raymond Feng <rfeng@us.ibm.com>
2017-02-08 11:16:20 -0800

    Switch to loopback-connector-grpc

--- note-loopback/common/models/note.js
+++ note-loopback/common/models/note.js
@@ -1,13 +1,14 @@
 module.exports = function(Note) {
-  var path = require('path');
   var fs = require('fs');
-  var PROTO_PATH = path.join(__dirname, '../../proto/note.proto');
-
-  var grpc = require('grpc');
-  var proto = grpc.load(PROTO_PATH);
 
   var zipkinAgent = require('../../lib/zipkin-agent');
 
+  /*
+  var path = require('path');
+  var grpc = require('grpc');
+  var PROTO_PATH = path.join(__dirname, '../../proto/note.proto');
+
+  var proto = grpc.load(PROTO_PATH);
   var rootCerts = fs.readFileSync(path.join(__dirname, '../../bin/grpc.crt'));
   var ssl = grpc.credentials.createSsl(rootCerts);
   var address = Note.settings.encryptionServiceAddress || 'localhost:50052';
@@ -23,6 +24,7 @@ module.exports = function(Note) {
     translationServiceAddress,
     grpc.credentials.createInsecure()
   );
+  */
 
   Note.observe('before save', function encryptContent(ctx, next) {
     console.log('Requesting to encrypt content: %s', ctx.instance.content);
@@ -30,13 +32,13 @@ module.exports = function(Note) {
     zipkinAgent.traceClient('note-loopback.encrypt',
       {zipkinServerUrl: Note.settings.zipkinServerUrl}, ctx.options,
       function(metadata, done) {
-        translationClient.translate(ctx.instance.toJSON(), metadata,
+        Note.app.models.TranslationClient.translate(ctx.instance.toJSON(), metadata,
           function(err, note) {
             if (err) {
               return done(err);
             }
             console.log('Content is now translated: %s', note.content);
-            encryptionClient.encrypt(note, metadata,
+            Note.app.models.EncryptionClient.encrypt(note, metadata,
               function(err, note) {
                 if (err) {
                   return done(err);
